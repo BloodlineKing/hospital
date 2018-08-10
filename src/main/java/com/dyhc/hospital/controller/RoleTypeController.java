f1c8ee65ae8158f31829151ac14bc293e7ea9cd1
BloodlineKing <1136599243@qq.com>
2018-08-10 20:53:01 +0800

    添加日志

--- src/main/java/com/dyhc/hospital/controller/RoleTypeController.java
+++ src/main/java/com/dyhc/hospital/controller/RoleTypeController.java
@@ -2,6 +2,8 @@ package com.dyhc.hospital.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.dyhc.hospital.service.RoleTypeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,12 @@ public class RoleTypeController {
     @Autowired
     private RoleTypeService roleTypeService;
 
+    private Logger logger= LoggerFactory.getLogger(RoleTypeController.class);
+
     @RequestMapping("listRoleType")
     @ResponseBody
     public String listRoleType(){
+        logger.error("查询所有角色类型");
         return JSON.toJSONString(roleTypeService.listRoleType());
     }
 }

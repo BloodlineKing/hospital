7e59c48576341cc495c9752c3d42189c6cbe073b
lwr <1195557074@qq.com>
2018-08-11 10:55:12 +0800

    李文荣-判断用户是否有存档 如果存在 就调用这些信息进行显示,如果没有就登记信息

--- src/main/java/com/dyhc/hospital/dao/ArchivesMapper.java
+++ src/main/java/com/dyhc/hospital/dao/ArchivesMapper.java
@@ -1,9 +1,16 @@
 package com.dyhc.hospital.dao;
 
+import com.dyhc.hospital.entity.Archives;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface ArchivesMapper {
+    /** 李文荣
+     *  判断用户是否有存档 如果存在 就调用这些信息进行显示,如果没有就登记信息
+     * @return
+     */
+    Archives ifArchives(@Param("testNumber") String testNumber );
 }
--- src/test/java/com/dyhc/hospital/HospitalApplicationTests.java
+++ src/test/java/com/dyhc/hospital/HospitalApplicationTests.java
@@ -1,6 +1,8 @@
 package com.dyhc.hospital;
 
+import com.dyhc.hospital.dao.ArchivesMapper;
 import com.dyhc.hospital.dao.UserRegisterInfoMapper;
+import com.dyhc.hospital.entity.Archives;
 import com.dyhc.hospital.entity.UserRegisterInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,9 +18,11 @@ public class HospitalApplicationTests {
 
     @Autowired
     public UserRegisterInfoMapper userRegisterInfoMapper;
+    @Autowired
+    private ArchivesMapper archivesMapper;
     @Test
     public void contextLoads() {
-
+        //archiveslwr();
     }
 
 
@@ -28,6 +32,14 @@ public class HospitalApplicationTests {
     public void userlwr(){
         List<UserRegisterInfo> list = userRegisterInfoMapper.findUserRegisterInfos("lh","17635692127");
         System.out.print("长度是=========>"+list.size());
+    }
+
+    /**
+     * 判断用户是否有存档 如果存在 就调用这些信息进行显示,如果没有就登记信息
+     */
+    public void archiveslwr(){
+        Archives info = archivesMapper.ifArchives("20180810001");
+        System.out.println("检查结果:"+info);
     }
 
 }

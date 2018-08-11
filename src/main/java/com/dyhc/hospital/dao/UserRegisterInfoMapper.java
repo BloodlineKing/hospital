bb9468d0c269dbb3f2508045c2b82a227d33a2e2
lwr <1195557074@qq.com>
2018-08-11 12:21:55 +0800

    李文荣-根据用户编号查看存档信息 存在这个用户 就把用户信息显示出来,并且检查次数加1==>updArchivesCheckCount()方法

--- src/main/java/com/dyhc/hospital/dao/UserRegisterInfoMapper.java
+++ src/main/java/com/dyhc/hospital/dao/UserRegisterInfoMapper.java
@@ -12,10 +12,20 @@ import java.util.List;
 public interface UserRegisterInfoMapper {
     /**
      * 李文荣
-     * 查询个人的预约记录
+     * 查询个人的预约记录 创建人 创建时间为空
      * @param nameCode  姓名简码
      * @param telephone  手机号
      * @return
      */
     public List<UserRegisterInfo> findUserRegisterInfos(@Param("nameCode") String nameCode, @Param("telephone")String telephone);
+
+    /**
+     *  李文荣
+     *  根据用户编号查看存档信息 存在这个用户 就把用户信息显示出来,并且检查次数加1==>updArchivesCheckCount()方法
+     * @param testNumber 用户编号
+     * @return 用户登记信息
+     */
+    public UserRegisterInfo findUserRegisterInfoByTestNumber(@Param("testNumber") String testNumber);
+
+
 }

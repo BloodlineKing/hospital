4687e98d5778ec2cde0c76e434602315b4923d5d
wjjzs <15035820946@163.com>
2018-08-14 15:02:16 +0800

    王娇

--- src/main/java/com/dyhc/hospital/dao/CostDetailMapper.java
+++ src/main/java/com/dyhc/hospital/dao/CostDetailMapper.java
@@ -11,21 +11,12 @@ import java.util.List;
 public interface CostDetailMapper {
 
    /**王娇娇
+    * 费用明细
     * 缴费 状态为0
-    * @param costDetail
-    * @return
-    */
-   int addCostDetailByPayment(CostDetail costDetail )throws Exception;
-   /**王娇娇
     * 扣费 状态为1
-    * @param costDetail
-    * @return
-    */
-   int addCostDetailByDeductions(CostDetail costDetail)throws Exception;
-   /**王娇娇
     * 退费 状态为2
     * @param costDetail
     * @return
     */
-   int addCostDetailByRefund(CostDetail costDetail)throws Exception;
+   int addCostDetail(CostDetail costDetail )throws Exception;
 }
--- src/main/java/com/dyhc/hospital/dao/RoleInfoMapper.java
+++ src/main/java/com/dyhc/hospital/dao/RoleInfoMapper.java
@@ -2,6 +2,7 @@ package com.dyhc.hospital.dao;
 
 import com.dyhc.hospital.entity.RoleInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,27 +15,27 @@ public interface RoleInfoMapper {
      * @param roleInfo
      * @return
      */
-    int addRoleInfo(RoleInfo roleInfo);
+    int addRoleInfo(RoleInfo roleInfo)throws Exception;
 
     /**
      * 修改角色信息
      * @param roleInfo
      * @return
      */
-    int updRoleInfo(RoleInfo roleInfo);
+    int updRoleInfo(RoleInfo roleInfo)throws Exception;
 
     /**
      * 获取所有角色信息
      * @return
      */
-    List<RoleInfo> getRoleInfoList();
+    List<RoleInfo> getRoleInfoList()throws Exception;
 
     /**
      * 根据角色编号查询该角色
      * @param roleInfoId
      * @return
      */
-    RoleInfo getRoleInfoById(Integer roleInfoId);
+    RoleInfo getRoleInfoById(@Param("roleInfoId") Integer roleInfoId)throws Exception;
 
 
 }
